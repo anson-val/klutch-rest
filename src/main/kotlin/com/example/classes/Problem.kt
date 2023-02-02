f6432f66c888f96df1dea03223b1831cd827f6a3
Anson Ng <914anson5@gmail.com>
2023-02-02 15:33:06 +0800

    refactor: split Problem and TestCase class constructors into multiple lines

--- src/main/kotlin/com/example/classes/Problem.kt
+++ src/main/kotlin/com/example/classes/Problem.kt
@@ -3,4 +3,9 @@ package com.example.classes
 import kotlinx.serialization.*
 
 @Serializable
-data class Problem (val id: String, val title: String, val description: String, val testCases: List<TestCase>)
+data class Problem (
+    val id: String,
+    val title: String,
+    val description: String,
+    val testCases: List<TestCase>
+)
--- src/main/kotlin/com/example/classes/TestCase.kt
+++ src/main/kotlin/com/example/classes/TestCase.kt
@@ -3,4 +3,10 @@ package com.example.classes
 import kotlinx.serialization.*
 
 @Serializable
-data class TestCase(val input: String, val expectedOutput: String, val comment: String, val weight: Double, val timeOutSeconds: Double)
+data class TestCase(
+    val input: String,
+    val expectedOutput: String,
+    val comment: String,
+    val weight: Double,
+    val timeOutSeconds: Double
+)
